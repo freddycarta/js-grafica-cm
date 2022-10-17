b5863c9ab5513bf621b8017c55d25d3cbbef84ba
freddycarta <cartafederico92@gmail.com>
2022-10-17 17:43:10 +0200

    consegna

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function creaGriglia() {
     resetGame()
     // genero la griglia
     console.log('creo griglia')
-    //ciclare 100 volte per creare 100 celle
+    //ciclare 100 volte per creare 100 celle con numeri
     for(let i = 0; i < 100; i++) {
         // console.log('cella n: ', i + 1)
         const cella = creaCella()
-
+        cella.innerHTML = i+1
         gridElement.append(cella)
     }
 
@@ -24,7 +24,15 @@ function creaCella() {
     // creo elemento del dom e aggiungo classe cella
     const el = document.createElement('div')
     el.classList.add('cella')
+    // aggiungo listener al click
+    el.addEventListener('click',onClick)
+
     return el
+}
+
+function onClick() {
+    console.log(this)
+    this.classList.add('clicked')
 }
 
 function resetGame() {
