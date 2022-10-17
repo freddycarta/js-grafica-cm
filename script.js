0f9f7ad31dcfdc8d58545d0ed0f28f52958289d3
freddycarta <cartafederico92@gmail.com>
2022-10-17 17:01:27 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log('hi');
+
+const playButtonEl = document.querySelector('.play-button')
+const gridElement = document.querySelector('.grid')
+playButtonEl.addEventListener('click',creaGriglia)
+
+function creaGriglia() {
+    // reset
+    resetGame()
+    // genero la griglia
+    console.log('creo griglia')
+    //ciclare 100 volte per creare 100 celle
+    for(let i = 0; i < 100; i++) {
+        // console.log('cella n: ', i + 1)
+        const cella = creaCella()
+
+        gridElement.append(cella)
+    }
+
+}
+
+
+function creaCella() {
+    // creo elemento del dom e aggiungo classe cella
+    const el = document.createElement('div')
+    el.classList.add('cella')
+    return el
+}
+
+function resetGame() {
+    // svuoto la griglia
+    gridElement.innerHTML = ''
+}
